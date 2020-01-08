364afa51849342cd52a9bc16dd054f74d625c144
Mark <plambiroo@gmail.com>
2020-01-08 13:59:07 +0300

    adding beer entity and beerService stub

---
+++ src/main/java/guru/springframework/msscbeerservice/web/services/BeerService.java
@@ -0,0 +1,19 @@
+package guru.springframework.msscbeerservice.web.services;
+
+import guru.springframework.msscbeerservice.web.model.BeerDto;
+
+import java.util.UUID;
+
+/**
+ * Created by jt on 2019-04-20.
+ */
+public interface BeerService {
+
+    BeerDto getBeerById(UUID beerId);
+
+    BeerDto saveNewBeer(BeerDto beerDto);
+
+    void updateBeer(UUID beerId, BeerDto beerDto);
+
+    void deleteBeerById(UUID beerId);
+}
---
+++ src/main/java/guru/springframework/msscbeerservice/web/services/BeerServiceImpl.java
@@ -0,0 +1,41 @@
+package guru.springframework.msscbeerservice.web.services;
+
+import guru.springframework.msscbeerservice.web.model.BeerDto;
+import guru.springframework.msscbeerservice.web.model.BeerStyle;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+/**
+ * Created by jt on 2019-04-20.
+ */
+@Slf4j
+@Service
+public class BeerServiceImpl implements BeerService {
+
+    @Override
+    public BeerDto getBeerById(UUID beerId) {
+        return BeerDto.builder().id(UUID.randomUUID())
+                .beerName("Galaxy Cat")
+                .beerStyle(BeerStyle.ALE)
+                .build();
+    }
+
+    @Override
+    public BeerDto saveNewBeer(BeerDto beerDto) {
+        return BeerDto.builder()
+                .id(UUID.randomUUID())
+                .build();
+    }
+
+    @Override
+    public void updateBeer(UUID beerId, BeerDto beerDto) {
+        //todo impl - would add a real impl to update beer
+    }
+
+    @Override
+    public void deleteBeerById(UUID beerId) {
+        log.debug("Deleting a beer with id {}", beerId);
+    }
+}
