a711c624405d21e7ecb6cccad7ac5d4a86fed3f5
Mark <plambiroo@gmail.com>
2020-01-19 14:49:15 +0300

    implementing brewing service to trigger Brewing

--- src/main/java/guru/springframework/msscbeerservice/repositories/BeerRepository.java
+++ src/main/java/guru/springframework/msscbeerservice/repositories/BeerRepository.java
@@ -4,11 +4,11 @@ import guru.springframework.msscbeerservice.domain.Beer;
 import guru.springframework.msscbeerservice.web.model.BeerStyle;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+public interface BeerRepository extends JpaRepository<Beer, UUID> {
 
     Page<Beer> findAllByBeerNameAndBeerStyle(String beerName, BeerStyle beerStyle, Pageable pageRequest);
 
---
+++ src/main/java/guru/springframework/msscbeerservice/services/BrewingService.java
@@ -0,0 +1,44 @@
+package guru.springframework.msscbeerservice.services;
+
+import guru.springframework.msscbeerservice.config.JmsConfig;
+import guru.springframework.msscbeerservice.domain.Beer;
+import guru.springframework.msscbeerservice.events.BrewBeerEvent;
+import guru.springframework.msscbeerservice.mappers.BeerMapper;
+import guru.springframework.msscbeerservice.repositories.BeerRepository;
+import guru.springframework.msscbeerservice.services.clients.inventory.BeerInventoryService;
+import guru.springframework.msscbeerservice.web.model.BeerDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class BrewingService {
+
+    private final BeerRepository beerRepository;
+    private final BeerInventoryService beerInventoryService;
+    private final JmsTemplate jmsTemplate;
+    private final BeerMapper beerMapper;
+
+    @Scheduled(fixedRate = 5000) //every 5 seconds
+    public void checkForLowInventory() {
+        List<Beer> beers = beerRepository.findAll();
+
+        beers.forEach(beer -> {
+            Integer inventoryQoh = beerInventoryService.getOnHandInventory(beer.getId());
+
+            log.debug("Min Onhand is: {}", beer.getMinOnHand());
+            log.debug("Inventory is: {}", beer.getMinOnHand());
+
+            if (beer.getMinOnHand() >= inventoryQoh) {
+                BeerDto beerDto = beerMapper.beerToBeerDto(beer);
+                jmsTemplate.convertAndSend(JmsConfig.BREWING_REQUEST_QUEUE, new BrewBeerEvent(beerDto));
+            }
+        });
+    }
+}
