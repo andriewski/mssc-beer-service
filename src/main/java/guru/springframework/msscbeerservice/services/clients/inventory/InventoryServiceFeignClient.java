e0e8b68d203e8ddce462496982a0ce47277237d4
Mark <plambiroo@gmail.com>
2020-08-06 19:57:26 +0300

    Adding OpenFeign to the project

---
+++ src/main/java/guru/springframework/msscbeerservice/services/clients/inventory/InventoryServiceFeignClient.java
@@ -0,0 +1,24 @@
+package guru.springframework.msscbeerservice.services.clients.inventory;
+
+import guru.springframework.msscbeerservice.services.clients.inventory.model.BeerInventoryDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@FeignClient(name = "inventory-service")
+public interface InventoryServiceFeignClient {
+
+    @RequestMapping(
+            method = RequestMethod.GET,
+            value = BeerInventoryServiceRestTemplateImpl.INVENTORY_PATH,
+            consumes = APPLICATION_JSON_VALUE
+    )
+    ResponseEntity<List<BeerInventoryDto>> getOnHandInventory(@PathVariable UUID beerId);
+}
